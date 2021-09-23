7cce9eb00f643c06bf952885af3ea318286b5f46
Albert <albert.bagil@gmail.com>
2021-09-23 14:53:22 +0700

    Tugas 20 Javascript
    Tugas 20 Javascript untuk dikumpulkan ke Niomic

---
+++ Tugas20.js
@@ -0,0 +1,7 @@
+function cari() {
+  var kata = "There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable.";
+  console.log(kata.match(/are/));
+  console.log("");
+  console.log(kata.match(/words/));
+}
+cari()
